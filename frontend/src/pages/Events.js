2223df205efeee3de332b6eb3f773f30f35e4af6
jydnbach <jaydenkim95@gmail.com>
2024-06-20 16:01:00 +0900

    error using json

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom'; //loader data also works in children components but not upward.
+import { useLoaderData, json } from 'react-router-dom'; //loader data also works in children components but not upward.
 
 import EventsList from '../components/EventsList';
 
@@ -19,9 +19,10 @@ export async function loader() {
   const res = await fetch('http://localhost:8080/events');
 
   if (!res.ok) {
-    throw new Response(
-      JSON.stringify({ message: 'Could not fetch data.' }, { status: 500 })
-    );
+    // throw new Response(
+    //   JSON.stringify({ message: 'Could not fetch data.' }, { status: 500 })
+    // );
+    throw json({ message: 'Could not fetch data.' }, { status: 500 });
   } else {
     return res; // return res immediately. .. where does res.json() go??
     // const data = await res.json();
