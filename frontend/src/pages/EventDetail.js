ab9143e1eec68b56bfe82d3856340ccffe535c2f
jydnbach <jaydenkim95@gmail.com>
2024-06-20 14:23:10 +0900

    routing events & eventDetail pages
    useparams

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 function EventDetailPage() {
-  return <h1>EventDetail</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>EventDetail</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
 }
 export default EventDetailPage;
