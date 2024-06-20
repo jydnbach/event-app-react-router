aa4a660092f307d25a27805fc4d66be44b480a2e
jydnbach <jaydenkim95@gmail.com>
2024-06-20 13:47:20 +0900

    navlinks paths set in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,17 +22,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
-import Home from './pages/Home';
-import NewEvent from './pages/NewEvent';
-import EventDetail from './pages/EventDetail';
-import Events from './pages/Events';
-import EditEvent from './pages/EditEvent';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import EventDetailPage from './pages/EventDetail';
+import EventsPage from './pages/Events';
+import EditEventPage from './pages/EditEvent';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/events', element: <EventsPage /> },
+      { path: '/events/:eventId', element: <EventDetailPage /> },
+      { path: '/events/new', element: <NewEventPage /> },
+      { path: '/events/:eventId', element: <EditEventPage /> },
+    ],
   },
 ]);
 
