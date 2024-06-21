eff340743db91d0b5010165a759e0b00bdd63884
jydnbach <jaydenkim95@gmail.com>
2024-06-21 14:58:08 +0900

    fix

--- frontend/src/pages/Newsletter.js
+++ frontend/src/pages/Newsletter.js
@@ -1,4 +1,4 @@
-import NewsletterSignup from '../components/aNewsletterSignup';
+import NewsletterSignup from '../components/NewsletterSignup';
 import PageContent from '../components/PageContent';
 
 function NewsletterPage() {
