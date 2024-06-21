7394bf31d8114c36ba3b16495a6c8dee5975bc0d
jydnbach <jaydenkim95@gmail.com>
2024-06-21 15:28:23 +0900

    events tab loading page
    Suspense, Await, defer

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,23 @@
-import { useLoaderData, json } from 'react-router-dom'; //loader data also works in children components but not upward.
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom'; //loader data also works in children components but not upward.
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
-  const { events } = data;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+export async function loadEvents() {
   const res = await fetch('http://localhost:8080/events');
 
   if (!res.ok) {
@@ -24,8 +26,16 @@ export async function loader() {
     // );
     throw json({ message: 'Could not fetch data.' }, { status: 500 });
   } else {
-    return res; // return res immediately. .. where does res.json() go??
+    // return res; // return res immediately. .. where does res.json() go??
     // const data = await res.json();
     // return data.events;
+    const data = await res.json();
+    return data.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
